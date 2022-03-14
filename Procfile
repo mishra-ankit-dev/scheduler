004edc8d6342e8f5a1e44419e1a0e8e7a5c0d56c
Ankit Mishra <amishm766@gmail.com>
2022-03-14 10:04:03 +0530

    Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c 'cd ./backend/ && daphne robofi.asgi:application --port $PORT --bind 0.0.0.0'
--- backend/run.ps1
+++ backend/run.ps1
@@ -1,3 +1,3 @@
-C:\.monitor\venv\Scripts\activate.ps1
-cd C:\.monitor\backend 
+D:\Learnings\Python\scheduler\venv\Scripts\activate.ps1
+cd D:\Learnings\Python\scheduler\backend 
 python manage.py runserver 0.0.0.0:8000
